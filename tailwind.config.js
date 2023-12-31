03eec17fd921b7f877808b1b377e70deae9d80f1
Amine derbal <aminedoran31@gmail.com>
2023-12-31 14:37:34 +0100

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   mode: 'jit',
   theme: {
+    screens: {
+      md: { max: '700px' },
+    },
+    colors: {
+      white: colors.white,
+    },
     extend: {
       backgroundImage: {
         hero: "url('/src/assets/hero.png')",
+      },
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
       },
     },
   },
