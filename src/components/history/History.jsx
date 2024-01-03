bbb9dca1724d2c7e199abcfbd93b9687c111f7af
Amine derbal <aminedoran31@gmail.com>
2024-01-03 09:33:24 +0100

    Add pictures

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -1,4 +1,5 @@
 import Header from './Header';
+import { photo1, photo2 } from '../../assets';
 
 const History = () => {
   return (
@@ -11,17 +12,41 @@ const History = () => {
               01.
             </h2>
             <div className="flex flex-col justify-e">
-              <p className="ml-[-1.5rem] md:ml-[-1rem] mt-[1rem] md:mt-[.5rem] text-[2rem] md:text-[1rem] font-bold text-primary">
+              <p className="ml-[-1.5rem] md:ml-[-1rem] mt-[.5rem] text-[2rem] md:text-[1rem] font-bold text-primary">
                 HISTORY
               </p>
             </div>
           </div>
-          <p className="pl-[30%] md:pl-[25%] md:text-[0.8rem] w-[80%] mt[-0.5rem]">
+          <p className="pl-[30%] md:pl-[25%] md:text-[0.8rem] w-[85%] mt[-0.5rem]">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab
             voluptatibus aspernatur ex animi, at, consequuntur commodi omnis
             maxime error nesciunt provident? Molestias deleniti suscipit
             eveniet, ut rem saepe quaerat consequuntur?
           </p>
+        </div>
+        <div className="bg-slider py-[1rem] px-[20%]">
+          <div className="my-0 mx-auto flex w-fit gap-[1rem] flex-wrap justify-around">
+            <img
+              src={photo1}
+              alt="1"
+              className="w-[180px] h-[120px] object-fill"
+            />
+            <img
+              src={photo2}
+              alt="2"
+              className="w-[180px] h-[120px] object-fill"
+            />
+            <img
+              src={photo1}
+              alt="1"
+              className="w-[180px] h-[120px] object-fill"
+            />
+            <img
+              src={photo2}
+              alt="2"
+              className="w-[180px] h-[120px] object-fill"
+            />
+          </div>
         </div>
       </div>
     </section>
